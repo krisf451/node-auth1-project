27c77c61b806cdb946131fb96f7dc63f3e0bbf32
Kristian Fulkerson <kristianf451@gmail.com>
2021-11-08 11:31:43 -0800

    users endpoint working, md also working for restricted access with no cookie

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,36 +1,13 @@
 const router = require("express").Router();
 const { restricted } = require("../auth/auth-middleware");
-const User = require("./users-model");
+const Users = require("./users-model");
 
-router.get("/", (req, res, next) => {
-  res.json("Users Router Get Request Working");
+router.get("/", restricted, (req, res, next) => {
+  Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch(next);
 });
 
 module.exports = router;
-
-// Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
-/**
-  [GET] /api/users
-
-  This endpoint is RESTRICTED: only authenticated clients
-  should have access.
-
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    },
-    // etc
-  ]
-
-  response on non-authenticated:
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
- */
-
-// Don't forget to add the router to the `exports` object so it can be required in other modules
